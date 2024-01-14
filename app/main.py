74e77c745a73683954d3307e3999b562de0e4b5a
Hakan Yücetürk <57252113+hakan8252@users.noreply.github.com>
2024-01-14 15:09:09 +0300

    health_check_only

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-from model import recommender_system
+# from model import recommender_system
 
 app = FastAPI()
 
@@ -10,9 +10,9 @@ def health_check():
     # For simplicity, always return 200 OK for this example
     return {"status": "ok"}
 
-@app.get("/recommend/{item_id}")
-async def get_recommendations(item_id: int, n_recommendation: int = 1):
-    # Call your recommender_system function with the provided item ID and number of recommendations
-    recommendations = recommender_system(item_id, n_recommendation)
-    # Return the recommendations in the response
-    return {"item_id": item_id, "recommendations": recommendations}
+# @app.get("/recommend/{item_id}")
+# async def get_recommendations(item_id: int, n_recommendation: int = 1):
+#     # Call your recommender_system function with the provided item ID and number of recommendations
+#     recommendations = recommender_system(item_id, n_recommendation)
+#     # Return the recommendations in the response
+#     return {"item_id": item_id, "recommendations": recommendations}
